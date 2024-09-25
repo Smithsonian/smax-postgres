e17633c2934be81e83c8ad46a291cfdc3fb8b906
Attila Kovacs <attipaci@gmail.com>
2024-09-25 15:26:35 +0200

    config.mk: Add LIB to LD_LIBRARY_PATH as needed.

--- config.mk
+++ config.mk
@@ -92,32 +92,37 @@ ifeq ($(SYSTEMD),1)
   LDFLAGS += -lsystemd
 endif
 
+# Search for libraries under LIB
+ifneq ($(findstring $(LIB),$(LD_LIBRARY_PATH)),$LIB)
+  LD_LIBRARY_PATH := $(LIB):$(LD_LIBRARY_PATH)
+endif
+
 # Compile and link against a specific PostgreSQL library (if defined)
 ifdef PGDIR
   CPPFLAGS += -I$(PGDIR)/include
   LDFLAGS += -L$(PGDIR)/lib
-  LD_LIBRARY_PATH = $(PGDIR)/lib:$(LD_LIBRARY_PATH)
+  LD_LIBRARY_PATH := $(PGDIR)/lib:$(LD_LIBRARY_PATH)
 endif
 
 # Compile and link against a specific smax library (if defined)
 ifdef SMAXLIB
   CPPFLAGS += -I$(SMAXLIB)/include
   LDFLAGS += -L$(SMAXLIB)/lib
-  LD_LIBRARY_PATH = $(SMAXLIB)/lib:$(LD_LIBRARY_PATH)
+  LD_LIBRARY_PATH := $(SMAXLIB)/lib:$(LD_LIBRARY_PATH)
 endif
 
 # Compile and link against a specific redisx library (if defined)
 ifdef REDISX
   CPPFLAGS += -I$(REDISX)/include
   LDFLAGS += -L$(REDISX)/lib
-  LD_LIBRARY_PATH = $(REDISX)/lib:$(LD_LIBRARY_PATH)
+  LD_LIBRARY_PATH := $(REDISX)/lib:$(LD_LIBRARY_PATH)
 endif
 
 # Compile and link against a specific xchange library (if defined)
 ifdef XCHANGE
   CPPFLAGS += -I$(XCHANGE)/include
   LDFLAGS += -L$(XCHANGE)/lib
-  LD_LIBRARY_PATH = $(XCHANGE)/lib:$(LD_LIBRARY_PATH)
+  LD_LIBRARY_PATH := $(XCHANGE)/lib:$(LD_LIBRARY_PATH)
 endif
 
 
